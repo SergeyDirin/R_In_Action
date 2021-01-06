2dac788172688ac5857fade05c3f61d8c254bb18
sdirin <sdirin@gmail.com>
2021-01-06 11:43:28 +0400

    Update

--- 2021-01-05 Chapter 12/Repeat C12.R
+++ 2021-01-05 Chapter 12/Repeat C12.R
@@ -22,3 +22,43 @@ anova(fit)
 
 fit <- aovp(weight ~ gesttime + dose, data=litter, perm="Prob")
 anova(fit)
+
+
+#bootstrap
+
+rsq <- function(formula, data, indices) {
+  d <- data[indices,]
+  fit <- lm(formula, data=d)
+  return(summary(fit)$r.square)
+}
+
+
+library(boot)
+set.seed(1234)
+results <- boot(data=mtcars, statistic=rsq,
+                R=1000, formula=mpg~wt+disp)
+
+print(results)
+
+plot(results)
+
+boot.ci(results, type=c("perc", "bca"))
+
+bs <- function(formula, data, indices) {
+  d <- data[indices,]
+  fit <- lm(formula, data=d)
+  return(coef(fit))
+}
+results <- boot(data=mtcars, statistic=bs,
+                R=1000, formula=mpg~wt+disp)
+print(results)
+plot(results, index=2)
+boot.ci(results, type="bca", index=2)
+
+
+
+
+
+
+
+
